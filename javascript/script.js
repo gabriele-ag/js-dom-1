e220862b4968f488f27006ef0a0b8cbfca7e86f4
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-04-28 15:19:46 +0200

    feat: create on and off light

--- javascript/script.js
+++ javascript/script.js
@@ -4,11 +4,21 @@ console.log(elementBtn)
 const imgElement = document.getElementById("lampadina")
 console.log(imgElement)
 
+let lampadaAccesa = false;
 
 elementBtn.addEventListener("click", function(){
-    imgElement.src = "./img/yellow_lamp 1.png";
-    elementBtn.innerText = "Accesa!";
-    elementBtn.style.backgroundColor = "yellow";
-    console.log("Si accende l'idea!");
+    if (lampadaAccesa) {
+        elementBtn.innerText = "Spenta"
+        elementBtn.style.backgroundColor = ""
+        imgElement.src = "./img/white_lamp 1.png"
+        console.log("Spenta")
+    } else {
+        elementBtn.innerText = "Accesa!"
+        elementBtn.style.backgroundColor = "yellow"
+        imgElement.src = "./img/yellow_lamp 1.png"
+        lampadaAccesa = true
+        console.log("Si accende l'idea!")
+
+    }
     
 })
